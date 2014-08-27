5fd318b76841a26846aa05c83d97311ccc39c932
Samuel Vaiter <samuel.vaiter@gmail.com>
2014-08-27 08:24:32 +0200

    add nuclear identifiability

--- locore/identifiability.py
+++ locore/identifiability.py
@@ -1,9 +1,8 @@
 from __future__ import division
 import numpy as np
-import scipy
+import scipy as sp
 
-from .operators import dual_prox, linf_l2_norm, l1_support, l1_l2_support,
-l1_l2_normalize_subvector
+from .operators import dual_prox, linf_l2_norm, l1_support, l1_l2_support, l1_l2_normalize_subvector
 from .algorithms import douglas_rachford
 from .utils import null, l1ball_projection
 
@@ -13,7 +12,7 @@ def crit_l1_synthesis(Phi, x):
     I = l1_support(x)
     J = ~I
     sI = np.sign(x)[I, :]
-    corr = np.dot(np.dot(Phi[:, J].T, scipy.linalg.pinv(Phi[:, I].T)), sI)
+    corr = np.dot(np.dot(Phi[:, J].T, sp.linalg.pinv(Phi[:, I].T)), sI)
     ic = np.max(np.abs(corr))
     res = {
         'ic': ic,
@@ -31,7 +30,7 @@ def _ic_minimization(DJ, Omega_x0, maxiter):
     Returns the solution of the problem
         min_{w \in Ker(D_J)} ||Omega sign(D_I^* x_0) - w||_\infty
     """
-    proj = np.dot(np.dot(DJ.T, scipy.linalg.pinv(np.dot(DJ, DJ.T))), DJ)
+    proj = np.dot(np.dot(DJ.T, sp.linalg.pinv(np.dot(DJ, DJ.T))), DJ)
     prox_indic = lambda w, la: w - np.dot(proj, w)
 
     proxinf = dual_prox(l1ball_projection)
@@ -59,13 +58,13 @@ def crit_l1_analysis(D, Phi, x):
     gram = np.dot(Phi.T, Phi)
     inside = np.dot(np.dot(U.T, gram), U)
     if np.prod(inside.shape) != 0:
-        inside = scipy.linalg.pinv(inside)
+        inside = sp.linalg.pinv(inside)
     A = np.dot(np.dot(U, inside), U.T)
-    Omega = np.dot(scipy.linalg.pinv(DJ), np.dot((np.eye(N) - np.dot(gram,
+    Omega = np.dot(sp.linalg.pinv(DJ), np.dot((np.eye(N) - np.dot(gram,
                                                                      A)), DI))
 
     # Compute wRC
-    wRC = scipy.linalg.norm(Omega, np.inf)
+    wRC = sp.linalg.norm(Omega, np.inf)
 
     # D-sign
     ds = np.sign(np.dot(DI.T, x))
@@ -103,7 +102,7 @@ def crit_l1_l2_synthesis(blocks, Phi, x):
     I = l1_l2_support(blocks, x)
     J = ~I
     e = l1_l2_normalize_subvector(blocks, I, x)
-    corr = np.dot(np.dot(Phi[:, J].T, scipy.linalg.pinv(Phi[:, I].T)), e)
+    corr = np.dot(np.dot(Phi[:, J].T, sp.linalg.pinv(Phi[:, I].T)), e)
     corr_extended = np.zeros(x.shape)
     corr_extended[I, :] = corr[I, :]
     ic = linf_l2_norm(blocks, corr_extended)
@@ -115,3 +114,64 @@ def crit_l1_l2_synthesis(blocks, Phi, x):
         'corr': corr
     }
     return res
+
+def crit_nuclear(Phi, x):
+    n1, n2 = x.shape
+    n = n1 * n2
+    
+    resh = lambda u: np.reshape(u, (n1, n2))
+    eps = 1e-8
+
+    # SVD of x: x=U*S*V'
+    U, S, V = sp.linalg.svd(x)
+    S = np.diag(S)
+    r = np.sum(S > eps)
+
+    # span orthogonal to row and col spaces
+    U0 = U[:,r:]
+    V0 = V[:,r:]
+    # space row and col spaces
+    U = U[:,:r]
+    V = V[:,:r]
+
+    # projector on S
+    #   P_S(Y) = U0*U0'*Y*V0*V0'
+    Vg = np.dot(V0, V0.T)
+    Ug = np.dot(U0, U0.T)
+    PS = np.kron(Vg, Ug)
+    PSmat = lambda y: np.dot(Ug, np.dot(y, Vg))
+
+    # T = {X ; U0*X*V0' = 0}
+    #   = Im(f) = { f(A,B) = U*A + B*V' ; A in R^{r x n2}, B in R^{n1 x r} }
+    PT = np.eye(n) - PS
+    # basis of T
+    B = np.concatenate((np.kron(np.eye(n2), U), np.kron(V, np.eye(n1))), axis=1)
+    
+    PhiT = np.dot(Phi, PT)
+    PhiB = np.dot(Phi, B)
+    # PhiT = Phi - PhiS
+
+    # generalized sign
+    e = np.dot(U, V.T)
+    e = e.reshape((n,1))
+
+    rPhiT = np.linalg.matrix_rank(PhiB);
+    # dim(T) = n1^2 - (n1-r)^2   (for square matrices)
+    # dimT = rank(PT);
+    dimT = n1 ** 2 - (n1-r) ** 2
+
+    if rPhiT < dimT:
+        return np.Inf
+    elif rPhiT == dimT:
+        # IC(X) = | PhiS' * PhiT^{+,*} * e |_{*,inf}    
+        # ic = norm( resh( PhiS' * pinv(PhiT)' * e )  );
+        
+        # Compute pinv(PhiT)' * e 
+        #    = Phi * B * pinv( B'*Phi'*Phi*B ) * B' * e(:)
+        v = np.dot(PhiB, np.dot(sp.linalg.pinv( np.dot(PhiB.T, PhiB) ), np.dot(B.T, e) ))
+        ic = sp.linalg.norm(PSmat(resh(np.dot(Phi.T, v))))
+        return ic
+    else:
+        raise Exception("injectivity problem")
+
+
